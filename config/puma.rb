d5b2c29a8573d6f43ba7ed8fe728570078a771bc
Fellipe Pontes <fellipepontesti@gmail.com>
2026-03-13 17:16:41 -0300

    adicionado a configuracao do puma

--- config/puma.rb
+++ config/puma.rb
@@ -1,44 +1,25 @@
-# This configuration file will be evaluated by Puma. The top-level methods that
-# are invoked here are part of Puma's configuration DSL. For more information
-# about methods provided by the DSL, see https://puma.io/puma/Puma/DSL.html.
-
-# Puma can serve each request in a thread from an internal thread pool.
-# The `threads` method setting takes two numbers: a minimum and maximum.
-# Any libraries that use thread pools should be configured to match
-# the maximum value specified for Puma. Default is set to 5 threads for minimum
-# and maximum; this matches the default thread size of Active Record.
+# Número de threads por worker
 max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
 threads min_threads_count, max_threads_count
 
-rails_env = ENV.fetch("RAILS_ENV") { "development" }
+# Ambiente do Rails
+environment ENV.fetch("RAILS_ENV") { "production" }
 
-if rails_env == "production"
-  # If you are running more than 1 thread per process, the workers count
-  # should be equal to the number of processors (CPU cores) in production.
-  #
-  # It defaults to 1 because it's impossible to reliably detect how many
-  # CPU cores are available. Make sure to set the `WEB_CONCURRENCY` environment
-  # variable to match the number of processors.
-  worker_count = Integer(ENV.fetch("WEB_CONCURRENCY") { 1 })
-  if worker_count > 1
-    workers worker_count
-  else
-    preload_app!
-  end
-end
-# Specifies the `worker_timeout` threshold that Puma will use to wait before
-# terminating a worker in development environments.
-worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
+# Número de workers (processos)
+workers ENV.fetch("WEB_CONCURRENCY") { 2 }
 
-# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-port ENV.fetch("PORT") { 3000 }
+# Carrega a aplicação antes de criar workers
+preload_app!
 
-# Specifies the `environment` that Puma will run in.
-environment rails_env
+# Socket usado pelo Nginx para conversar com o Puma
+bind "unix:///home/upontes/orangScore/tmp/sockets/puma.sock"
 
-# Specifies the `pidfile` that Puma will use.
-pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
+# Arquivo que guarda o PID do processo
+pidfile "/home/upontes/orangScore/tmp/pids/puma.pid"
 
-# Allow puma to be restarted by `bin/rails restart` command.
+# Arquivo de estado do Puma
+state_path "/home/upontes/orangScore/tmp/pids/puma.state"
+
+# Permite reiniciar com `rails restart`
 plugin :tmp_restart
