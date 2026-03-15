6289eb070a714abdb058443ed046faa13b92c919
Fellipe Pontes <fellipepontesti@gmail.com>
2026-03-15 10:52:49 -0300

    resend

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,4 @@
 require "active_support/core_ext/integer/time"
-require "resend"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
@@ -89,7 +88,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.delivery_method = :resend
-  config.action_mailer.resend_settings = {
+  config.action_mailer.delivery_method_options = {
     api_key: ENV['RESEND_API_KEY']
   }
 
