555a2860a2e36f60586be47f236d8f74c743c57c
Fellipe Pontes <fellipepontesti@gmail.com>
2026-05-03 17:39:20 -0300

    ajustando a configuracao do host do devise

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,11 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "noreply@orangscore.com.br"
+  default from: 'no-reply@orangscore.com.br'
   layout "mailer"
+
+  def default_url_options
+    {
+      host: 'orangscore.com.br',
+      protocol: 'https'
+    }
+  end
 end
