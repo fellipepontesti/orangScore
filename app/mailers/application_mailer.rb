b0afe9659b1f13e90e24606d578cd70464a26972
Fellipe Pontes <fellipepontesti@gmail.com>
2026-05-05 19:51:54 -0300

    adicionado ajuste no applicationmails

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,11 +1,6 @@
 class ApplicationMailer < ActionMailer::Base
-  helper_method :app_url
   default from: 'no-reply@orangscore.com.br'
   layout "mailer"
-
-  def app_url
-    "https://#{ENV.fetch('APP_HOST', 'orangscore.com.br')}"
-  end
 
   def default_url_options
     {
