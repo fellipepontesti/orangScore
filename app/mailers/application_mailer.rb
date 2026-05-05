4fbbb44d7052098db8cd4c7a73def248c0564f2f
Fellipe Pontes <fellipepontesti@gmail.com>
2026-05-05 19:16:12 -0300

    adicionado ajuste no caminho do confirmar conta

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,6 +2,10 @@ class ApplicationMailer < ActionMailer::Base
   default from: 'no-reply@orangscore.com.br'
   layout "mailer"
 
+  def app_url
+    ENV.fetch('APP_URL', 'https://orangscore.com.br')
+  end
+
   def default_url_options
     {
       host: 'orangscore.com.br',
