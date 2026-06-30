9cefe9866ee64b659718560c801bd1f4d6016936
Fellipe Pontes <fellipepontesti@gmail.com>
2026-06-30 14:16:51 -0300

    Configura padrao de 2 workers do Puma em producao

--- config/puma.rb
+++ config/puma.rb
@@ -7,7 +7,11 @@ threads min_threads_count, max_threads_count
 environment ENV.fetch("RAILS_ENV") { "development" }
 
 # Número de workers (processos)
-workers ENV.fetch("WEB_CONCURRENCY") { 0 }
+if ENV.fetch("RAILS_ENV", "development") == "production"
+  workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+else
+  workers ENV.fetch("WEB_CONCURRENCY") { 0 }
+end
 
 # Carrega a aplicação antes de criar workers
 preload_app!
